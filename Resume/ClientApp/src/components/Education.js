42dfe7be3e34e6df7ab6986fd7cb83f83179af7e
isabelladrest <iras@itu.dk>
2021-10-11 22:57:05 +0200

    Added my educational work

--- Resume/ClientApp/src/components/Education.js
+++ Resume/ClientApp/src/components/Education.js
@@ -25,81 +25,51 @@ export class Education extends Component {
                 <VerticalTimeline>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
-                        date="2011 - present"
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        date="2019 - present"
+                        iconStyle={{ background: '#2B3242', color: '#fff' }}
                     >
-                        <h3 className="vertical-timeline-element-title">Creative Director</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+                        <h3 className="vertical-timeline-element-title">Bachelor in Software Development</h3>
+                        <h4 className="vertical-timeline-element-subtitle">IT-University of Copenhagen, Copenhagen</h4>
                         <p>
-                        Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                        Following courses in both programming, design and business classes.
                         </p>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
-                        date="2010 - 2011"
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        date="2019 - 2019"
+                        iconStyle={{ background: '#2B3242', color: '#fff' }}
                     >
-                        <h3 className="vertical-timeline-element-title">Art Director</h3>
-                        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                        <h3 className="vertical-timeline-element-title">Mathematics Level A</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Extra course, Online</h4>
                         <p>
-                        Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                        Upgraded my mathematics level to A by an online course, while living in Paris.
                         </p>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
-                        date="2008 - 2010"
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        date="2015 - 2018"
+                        iconStyle={{ background: '#2B3242', color: '#fff' }}
                     >
-                        <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+                        <h3 className="vertical-timeline-element-title">High School</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Tornbjerg Gymnasium, Odense</h4>
                         <p>
-                        User Experience, Visual Design
+                        Majors in Mathematics, English and Socialstudies. A part of 'Tornbjerg Elite' and 'Team Danmark', which I qualified to by being an elite swimmer. 
                         </p>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
-                        date="2006 - 2008"
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        date="2005 - 2015"
+                        iconStyle={{ background: '#2B3242', color: '#fff' }}
                     >
-                        <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                        <h3 className="vertical-timeline-element-title">Primary School</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Aadalskolen, Odense</h4>
                         <p>
-                        User Experience, Visual Design
+                        Primary school from 0th to the 9th grade.
                         </p>
                     </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        date="April 2013"
-                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                    >
-                        <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-                        <p>
-                        Strategy, Social Media
-                        </p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        date="November 2012"
-                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                    >
-                        <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-                        <p>
-                        Creative Direction, User Experience, Visual Design
-                        </p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        date="2002 - 2006"
-                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                    >
-                        <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-                        <p>
-                        Creative Direction, Visual Design
-                        </p>
-                    </VerticalTimelineElement>
+                    
+                    
+                    
                 </VerticalTimeline>
              </div>  
       </div>
